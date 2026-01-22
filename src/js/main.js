6b7fda0dd68c896e7ee374922b15c6154eefa8b2
Guevarachirino <chi23017@byui.edu>
2026-01-22 10:48:11 -0500

    alert file

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
+import Alert from './alert.js';
+
 loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
@@ -9,3 +11,6 @@ const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
 
 productList.init();
+
+const alert = new Alert("/json/alerts.json");
+alert.loadAndDisplayAlerts();

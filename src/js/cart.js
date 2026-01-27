1148132e8e403823f061a48cea3dee4b988f87fa
Guevarachirino <chi23017@byui.edu>
2026-01-27 00:49:09 -0500

    cart quantities

--- src/js/cart.js
+++ src/js/cart.js
@@ -13,35 +13,66 @@ function renderCartContents() {
   productList.innerHTML = htmlItems.join('');
 
   renderCartTotal(cartItems);
+  addQuantityListeners();
 }
 
 function renderCartTotal(cartItems) {
   const cartFooter = document.querySelector('.cart-footer');
   const cartTotalElement = document.querySelector('.cart-total');
 
-  const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
+  const total = cartItems.reduce(
+    (sum, item) => sum + item.FinalPrice * item.quantity, 0);
 
   cartTotalElement.innerHTML = `Total: $${total.toFixed(2)}`;
   cartFooter.classList.remove('hide');
 }
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
 
-  return newItem;
+function cartItemTemplate(item) {
+  return `
+  <li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}" />
+    </a>
+
+    <h2 class="card__name">${item.Name}</h2>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+
+    <label>
+      Qty:
+      <input 
+        type="number"
+        min="1"
+        value="${item.quantity}"
+        data-id="${item.Id}"
+        class="cart-qty"
+      />
+    </label>
+
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
+}
+
+function addQuantityListeners() {
+  document.querySelectorAll('.cart-qty').forEach((input) => {
+    input.addEventListener('change', (e) => {
+      const id = e.target.dataset.id;
+      const quantity = parseInt(e.target.value);
+      updateCartQuantity(id, quantity);
+    });
+  });
+}
+
+function updateCartQuantity(id, quantity) {
+  const cartItems = getLocalStorage('so-cart') || [];
+
+  const item = cartItems.find((i) => i.Id === id);
+  if (item && quantity > 0) {
+    item.quantity = quantity;
+  }
+
+  localStorage.setItem('so-cart', JSON.stringify(cartItems));
+  renderCartContents();
 }
 
 renderCartContents();
